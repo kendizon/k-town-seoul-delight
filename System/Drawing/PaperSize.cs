1667153165fcbd668a1c9251eac803abba05839b
John Kenneth Dizon <156899013+kendizon@users.noreply.github.com>
2026-05-17 03:34:19 +0800

    Upload System Folder

---
+++ System/Drawing/PaperSize.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+
+namespace System.Drawing
+{
+    internal class PaperSize : Printing.PaperSize
+    {
+        public PaperSize(string name, int width, int height) : base(name, width, height)
+        {
+        }
+    }
+}
